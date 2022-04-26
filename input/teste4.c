47c39b5187d7deaf801105144b1926ec2fb2024f
frandepaula <58179210+frandepaula@users.noreply.github.com>
2022-04-26 12:11:37 -0300

    Add files via upload

--- input/teste4.c
+++ input/teste4.c
@@ -1,8 +1,8 @@
 int main()
 {
-  for == != sdnf er lkdkf_90 e34 <= >= - / * + 
-  int x, y = 1 < 3 >= 2 + 3*4 && 1, x, y;
+  int x, y = 1 || 2 + 3*4 && 1;
   float z = x > 3 && y <= 1 || y > x;
+  k = x > 3 && ((y <= 1) + 3 - 5 * 7) || y > x;
   while (x < y + z * 9 - 3)
   {
     int b = 1, c, d = 5;
@@ -19,8 +19,13 @@ int main()
       int j;
       while (t > 3)
         print(1);
+      {
+        C = zz < ww;
+        D = xx >= yy;
+      }
     }
     float k;
   }
   int z;
+  float x, y;
 }
